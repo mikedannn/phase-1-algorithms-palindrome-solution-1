06dd808420d01b1745ead5c8909a41b8e2754c14
Michael <michael.daniels1013@gmail.com>
2022-03-06 15:53:57 -0600

    commit tree 502e4aa110e86f3796ecbf6144d4bad2709611a7

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  const wordArray = word.split('');
+  const reverseWordArray = wordArray.reverse();
+  const reversedWord = reverseWordArray.join('');
+  return reversedWord;
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  if(word ===reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
 /* 
-  Add your pseudocode here
+  reverse the entered string 
+  if the reversed string equals the input string
+    return true
+  if else
+    return false
 */
 
 /*
@@ -15,6 +32,26 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("rotor"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("bubblegum"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("tiny"));
 
   console.log("");
 
